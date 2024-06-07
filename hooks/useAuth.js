e60d01522859065279b1177e0e5ff23f05fb82f4
tongkiankiat <tong.kiankiat@gmail.com>
2024-06-07 19:06:15 +0800

    Added useAuth.js to handle authentication logic

---
+++ hooks/useAuth.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { firebase_auth } from '../config/firebaseConfig';
+
+
+export default function useAuth() {
+    const [user, setUser] = useState(null);
+    useEffect(() => {
+        const unsub = onAuthStateChanged(firebase_auth, user => {
+
+            if (user) {
+                setUser(user);
+            } else {
+                setUser(null);
+            }
+        });
+        return unsub;
+    }, [])
+    return {user};
+}
