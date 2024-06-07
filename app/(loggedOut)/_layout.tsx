0781642a44ac2574cdad73308176ed0a2e3666d4
tongkiankiat <tong.kiankiat@gmail.com>
2024-06-07 19:04:13 +0800

    Updated _layout for loggedOut screens

--- app/(loggedOut)/_layout.tsx
+++ app/(loggedOut)/_layout.tsx
@@ -3,7 +3,9 @@ import { Stack } from "expo-router";
 const loggedInLayout = () => {
     return (
         <Stack screenOptions={{headerShown:false}}>
-            <Stack.Screen name="homescreen"/>
+            <Stack.Screen name="loginscreen"/>
+            <Stack.Screen name="registerscreen"/>
+            <Stack.Screen name="registrationdetails"/>
         </Stack>
     )
 }
