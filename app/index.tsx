c71df16542309e12d86aff190fe581be2ebf4350
tongkiankiat <tong.kiankiat@gmail.com>
2024-06-07 19:02:10 +0800

    Renamed homescreen.tsx to index.tsx and treated index.tsx as mainscreen

---
+++ app/index.tsx
@@ -0,0 +1,45 @@
+import { router } from 'expo-router'
+import React from 'react'
+import { View, StyleSheet, Text, TouchableOpacity, Image} from 'react-native'
+
+const HomeScreen = () => {
+    return (
+      <View style={styles.container}>
+          <Image source={require('../assets/images/Logo.png')} style = {styles.logo}/>
+          <TouchableOpacity style={styles.button} onPress={() => router.push('/loginscreen')}>
+              <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => router.push('/registerscreen')}>
+              <Text style={styles.buttonText}>Register</Text>
+          </TouchableOpacity>
+      </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#E4FBFF',
+    },
+    button: {
+      backgroundColor: '#007bff',
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+      borderRadius: 5,
+      marginVertical: 10,
+    },
+    buttonText: {
+      color: 'white',
+      fontSize: 16,
+      fontWeight: 'bold',
+    },
+    logo: {
+      width: 200,
+      height: 200,
+      resizeMode: 'stretch',
+    }
+})
+
+export default HomeScreen;
