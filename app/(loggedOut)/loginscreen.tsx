441b73b1561e5c318fd1dddb44b4b7b0acca8742
tongkiankiat <tong.kiankiat@gmail.com>
2024-06-30 22:45:50 +0800

    Added keyboardavoidingview

--- app/(loggedOut)/loginscreen.tsx
+++ app/(loggedOut)/loginscreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, TextInput, Text, StyleSheet, TouchableOpacity, ActivityIndicator, AppState} from 'react-native';
+import { View, TextInput, Text, StyleSheet, TouchableOpacity, ActivityIndicator, AppState, Alert, KeyboardAvoidingView } from 'react-native';
 import { router } from 'expo-router';
 import { supabase } from '../../lib/supabase'
 
@@ -22,47 +22,52 @@ const LogInScreen = () => {
       email: email,
       password: password,
     });
-    if (error) { 
+    if (error) {
       alert(error.message);
     }
     else {
-      alert("Successfully logged in!");
-      router.push('../(loggedIn)/home');
+      Alert.alert('Success!', 'Successfully logged in!', [{ text: 'OK', onPress: () => router.push('../(loggedIn)/home') }]);
     }
     setLoading(false);
   }
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        placeholderTextColor="#888"
-        value={email}
-        onChangeText={(value) => setEmail(value)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        placeholderTextColor="#888"
-        secureTextEntry
-        value={password}
-        onChangeText={(value) => setPassword(value)}
-      />
-      {
-        loading ? (
-          <ActivityIndicator size="large" color="#0000ff" />
-        ) : (
-          <TouchableOpacity style={styles.button} onPress={signIn}>
-            <Text style={styles.buttonText}>Login</Text>
-          </TouchableOpacity>
-        )
-      }
-    </View>
+    <KeyboardAvoidingView style={styles.keyboardcontainer}>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder='Email'
+          placeholderTextColor='#888'
+          value={email}
+          onChangeText={(value) => setEmail(value)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder='Password'
+          placeholderTextColor='#888'
+          secureTextEntry
+          value={password}
+          onChangeText={(value) => setPassword(value)}
+        />
+        {
+          loading ? (
+            <ActivityIndicator size='large' color='#0000ff' />
+          ) : (
+            <TouchableOpacity style={styles.button} onPress={signIn}>
+              <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+          )
+        }
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
+  keyboardcontainer: {
+    flex: 1,
+    backgroundColor: '#E4FBFF'
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
