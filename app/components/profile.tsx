e364c6996325066ef206c9ff76ee3bd06e50f36e
tongkiankiat <tong.kiankiat@gmail.com>
2024-07-01 00:50:49 +0800

    Updated Profile to include Logout function

--- app/components/profile.tsx
+++ app/components/profile.tsx
@@ -1,12 +1,42 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { supabase } from '../../lib/supabase';
+import { router } from 'expo-router';
 
 const profile = () => {
+  // signing out the user
+  const logOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    router.push('/');
+  };
+
   return (
-    <View>
-      <Text>profile</Text>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={logOut}>
+          <Text style={styles.buttonText}>Logout</Text>
+        </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#007bff',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginVertical: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  }
+})
 
 export default profile
