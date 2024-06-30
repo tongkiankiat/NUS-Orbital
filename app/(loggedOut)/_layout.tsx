74724976a85bfe81a7ded80180346e94c559d505
tongkiankiat <tong.kiankiat@gmail.com>
2024-06-30 22:45:27 +0800

    Updated code styling

--- app/(loggedOut)/_layout.tsx
+++ app/(loggedOut)/_layout.tsx
@@ -1,11 +1,11 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
 
 const loggedInLayout = () => {
     return (
         <Stack screenOptions={{headerShown:false}}>
-            <Stack.Screen name="loginscreen"/>
-            <Stack.Screen name="registerscreen"/>
-            <Stack.Screen name="registrationdetails"/>
+            <Stack.Screen name='loginscreen'/>
+            <Stack.Screen name='registerscreen'/>
+            <Stack.Screen name='registrationdetails'/>
         </Stack>
     )
 }
