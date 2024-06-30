884e78486ade804ca89ca2b9bc42b1f204d41370
tongkiankiat <tong.kiankiat@gmail.com>
2024-06-30 22:41:53 +0800

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,7 @@
-import { Stack } from "expo-router";
-import React, { useState } from "react";
-import { ActivityIndicator, View, StyleSheet } from "react-native";
-import useAuth from "../hooks/useAuth";
+import { Stack } from 'expo-router';
+import React, { useState } from 'react';
+import { ActivityIndicator, View, StyleSheet } from 'react-native';
+import useAuth from '../hooks/useAuth';
 
 const RootLayout = () => {
     // const [loading, setLoading] = useState(true);
@@ -11,22 +11,22 @@ const RootLayout = () => {
     // if (loading) {
     //     return (
     //         <View style={styles.loadingContainer}>
-    //             <ActivityIndicator size="large" color="#0000ff" />
+    //             <ActivityIndicator size='large' color='#0000ff' />
     //         </View>
     //     );
     // }
     // return (
     //     <Stack screenOptions={{headerShown:false}}>
     //         {user ? (
-    //             <Stack.Screen name="(loggedIn)" options={{ headerShown: false }} />
+    //             <Stack.Screen name='(loggedIn)' options={{ headerShown: false }} />
     //         ) : (
-    //             <Stack.Screen name="(loggedOut)" options={{ headerShown: false }} />
+    //             <Stack.Screen name='(loggedOut)' options={{ headerShown: false }} />
     //         )}
     //     </Stack>
     // );
     return (
         <Stack screenOptions={{headerShown:false}}>
-            <Stack.Screen name="index" options={{headerShown:false}}/>
+            <Stack.Screen name='index' options={{headerShown:false}}/>
         </Stack>
     )
 };
