3863aedf10dc3de826884508b2df0f18583d4461
tongkiankiat <tong.kiankiat@gmail.com>
2024-07-29 00:41:36 +0800

    Changed sharedheader icons

--- client/app/components/sharedheader.tsx
+++ client/app/components/sharedheader.tsx
@@ -1,22 +1,25 @@
-import { View, Text, StatusBar, TouchableOpacity, StyleSheet } from 'react-native'
-import React, { useContext } from 'react'
-import { Ionicons } from '@expo/vector-icons';
+import { View, Text, StatusBar, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { Ionicons, FontAwesome5 } from '@expo/vector-icons';
 import { router } from 'expo-router';
 import { UsernameContext } from '../context/UsernameContext';
+import { CoinsContext } from '../context/CoinsContext';
 
 const SharedHeader = () => {
   // Retrieve username
   const username = useContext(UsernameContext);
+  const { coins, setCoins } = useContext(CoinsContext);
 
   return (
     <View style={styles.header}>
-      <TouchableOpacity>
-        <Ionicons name='notifications' size={24} color='black' />
+      <TouchableOpacity onPress={() => router.push('../components/profile')}>
+        <Ionicons name='person-circle' size={26} color='white' />
       </TouchableOpacity>
       <Text style={styles.welcomeText}>Welcome, {username}</Text>
-      <TouchableOpacity onPress={() => router.push('../components/profile')}>
-        <Ionicons name='person-circle' size={26} color='black' />
-      </TouchableOpacity>
+      <View style={styles.coinsborder}>
+        <FontAwesome5 name="coins" size={20} color="gold" />
+        <Text style={{color: 'white'}}>{coins}</Text>
+      </View>
     </View>
   )
 };
@@ -30,13 +33,24 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: '#CCCCCC',
     paddingTop: StatusBar.currentHeight,
-    backgroundColor: '#092840'
+    backgroundColor: 'blue'
   },
   welcomeText: {
     fontSize: 18,
     fontWeight: 'bold',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    paddingLeft:60,
+    color: 'white'
   },
+  coinsborder: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: 'black',
+    width: 80
+  }
 });
 
 export default SharedHeader;
